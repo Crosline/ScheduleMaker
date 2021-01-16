a000f209117b1f0f455aaf4ab19f8a492bdbdf68
Crosline <cagkancaglayanel@gmail.com>
2021-01-17 00:43:42 +0300

    Scramblere - Main read

--- Scramblere.py
+++ Scramblere.py
@@ -1,3 +1,5 @@
+import random as rnd
+
 def open_file(txt):
     with open(txt, "r") as f:
         x = f.readlines()
@@ -8,17 +10,51 @@ def open_file(txt):
         y = []
         for j in i:
             t = j.split(" ")
-            t[1] = int(t[1])
 
-            while t[1] > 2:
-                y.append([t[0], 2])
-                t[1] -= 2
-            y.append([t[0], t[1]])
+            for i in range(int(t[1])):
+                y.append([t[0], 1])
 
         z.append(y)
 
     return z
 
 
+def scrambeler(lst, day, hour):
+    lessons = []
+    rnd.shuffle(lst)
+    tempList = lst.copy()
 
-print(open_file("TestInput.txt"))
+    i = len(tempList) - 1
+
+    for hour_index in range(day):
+
+        lessons_per_day = []
+        temphour = 0
+
+        while True:
+            temphour += tempList[i][1]
+            if(temphour > hour[hour_index]):
+                break
+            lessons_per_day.append(tempList[i])
+            i -= 1
+        lessons.append(lessons_per_day)
+
+    return lessons
+
+
+day = int(input("How many days do you take lessons?"))
+print("Enter your daily lesson hour")
+
+hour = []
+hours = 0
+
+for i in range(day):
+    hour.append(int(input("Day " + str(len(hour)+1))))
+    hours += hour[i]
+
+lst = open_file("TestInput.txt")
+lst = lst[0]
+print(lst)
+
+print(scrambeler(lst, day, hour))
+
