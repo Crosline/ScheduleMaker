053e32b637cc222ea78545ec499251c150f690a2
Crosline <cagkancaglayanel@gmail.com>
2021-01-17 15:26:13 +0300

    Switched to genetic algorithm
    Data required to gathered from .xlsl file

---
+++ GeneticAlgorithm/main.py
@@ -0,0 +1,26 @@
+
+ROOMS = \
+    [["R1", 25],
+     ["R2", 45],
+     ["R3", 35]]
+
+MEETING_TIMES = \
+    [["MT1", "MWF 09:00 - 10:00"],
+     ["MT2", "MWF 10:00 - 11:00"],
+     ["MT3", "TTH 09:00 - 10:30"],
+     ["MT4", "TTH 10:30 - 12:00"]]
+
+INSTRUCTORS = \
+    [["I1", "YGT YVZ", ["MATH", "PHYS"]],
+     ["I2", "EIN AIS", ["MATH", "CHEM"]],
+     ["I3", "STEVE JOBS", ["MATH"]],
+     ["I4", "EKIN DENIZ", ["BBM"]]]
+
+COURSES = \
+    [["C1", "MATH", "CS", 25],
+     ["C2", "MATH", "EE", 35],
+     ["C3", "PHYS", "CS", 20],
+     ["C4", "PHYS", "EE", 30],
+     ["C5", "ELE", "EE", 40],
+     ["C6", "ELE", "EE", 25],
+     ["C7", "BBM", "CS", 20]]
